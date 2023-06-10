6403b0b7fe5b65afb80807b5ef9e6cec73808405
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-06-11 01:52:48 +0200

    .

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import user from '../user';
+
+export const Profile = props => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={user.avatar} alt="User avatar" class="avatar" />
+        <p class="name">{user.username}</p>
+        <p class="tag">{user.tag}</p>
+        <p class="location">{user.location}</p>
+        <ul class="stats">
+          <li>
+            <span class="label">Followers</span>
+            <span class="quantity">{user.stats.followers}</span>
+          </li>
+          <li>
+            <span class="label">Views</span>
+            <span class="quantity">{user.stats.views}</span>
+          </li>
+          <li>
+            <span class="label">Likes</span>
+            <span class="quantity">{user.stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// ReactDOM.createRoot(document.getElementById('root')).render(profile);
