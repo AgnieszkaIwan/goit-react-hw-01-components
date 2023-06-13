173acf53f887d29f9c112fce79100d558d71f236
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-06-13 22:30:52 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import { Profile } from 'components/Profile';
+import { ProfileComponent } from 'components/Profile';
+import { Statistics } from 'components/Statistics';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile />
+    <ProfileComponent />
+    <Statistics title="Upload stats" stats={data} />
   </React.StrictMode>
 );
