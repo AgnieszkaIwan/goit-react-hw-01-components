4b75128c3e675bf823340ea295ee4a2494d480ae
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-06-14 23:59:51 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { Statistics } from 'components/Statistics';
 import data from '../data.json';
 import { FriendList } from 'components/FriendList';
 import friends from '../friends.json';
+import { TransactionHistory } from 'components/TransactionHistory';
+import transactions from '../transactions.json';
 
 export const App = () => {
   return (
@@ -17,13 +19,22 @@ export const App = () => {
       }}
     >
       <div>
+        <h1>Profile Component</h1>
         <ProfileComponent />
       </div>
       <div>
+        <h1>Statistics</h1>
         <Statistics title="Upload stats" stats={data} />
       </div>
       <div>
+        <h1>Friend List</h1>
         <FriendList friends={friends} />
+      </div>
+      <div>
+        <div>
+          <h1>Transaction History</h1>
+          <TransactionHistory items={transactions} />
+        </div>
       </div>
     </div>
   );
