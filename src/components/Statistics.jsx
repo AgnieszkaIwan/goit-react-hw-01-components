9b4c2ab53211f39768998b44cbdfac9c16872b50
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-06-19 22:03:57 +0200

    StstCSS

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,18 +1,20 @@
+import styles from '../components/Statistics.module.css';
+
 export const Statistics = ({ title, stats }) => {
   if (!stats || stats.length === 0) {
     return null; // Jeśli dane są puste lub niezdefiniowane, zwracamy null lub komunikat o ładowaniu
   }
 
   return (
-    <div className="statDataContainer">
-      <section className="statistics">
-        {title && <h2 className="title">{title}</h2>}
+    <div className={styles.statDataContainer}>
+      <section className={styles.statistics}>
+        {title && <h2 className={styles.title}>{title}</h2>}
 
-        <ul className="stat-list">
+        <ul className={styles.statList}>
           {stats.map(stats => (
-            <li className="item" key={stats.id}>
-              <span className="label">{stats.label}</span>
-              <span class="percentage">{stats.percentage}%</span>
+            <li className={styles.item} key={stats.id}>
+              <span className={styles.label}>{stats.label}</span>
+              <span class={styles.percentage}>{stats.percentage}%</span>
             </li>
           ))}
         </ul>
